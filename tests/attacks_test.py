8b562dc330f40070733d15430cde3750b6a791fe
Sazonov_ISP <sazonovg@ispras.ru>
2025-05-27 19:52:33 +0300

    Add MI Attack with shadow model

--- tests/attacks_test.py
+++ tests/attacks_test.py
@@ -155,6 +155,40 @@ class AttacksTest(unittest.TestCase):
                                                                           Metric("Accuracy", mask=mask_loc)])
         print(metric_loc)
 
+    def test_mi_shadow(self):
+        mi_attack_config = ConfigPattern(
+            _class_name="ShadowModelMIAttacker",
+            _import_path=MI_ATTACK_PARAMETERS_PATH,
+            _config_class="MIAttackConfig",
+            _config_kwargs={
+            }
+        )
+
+        gcn_gcn_sg_example = model_configs_zoo(dataset=self.gen_dataset_sg_example, model_name='gcn_gcn')
+
+        gnn_model_manager_sg_example = FrameworkGNNModelManager(
+            gnn=gcn_gcn_sg_example,
+            dataset_path=self.results_dataset_path_sg_example,
+            modification=self.default_config,
+            manager_config=self.manager_config,
+        )
+
+        gnn_model_manager_sg_example.set_mi_attacker(mi_attack_config=mi_attack_config)
+
+        attack_cnt = 2
+        # seed = 42
+        seed = None
+        if seed is not None:
+            np.random.seed(seed)
+        target_list = np.random.choice(self.gen_dataset_sg_example.dataset.data.x.shape[0], size=attack_cnt, replace=False)
+
+        gnn_model_manager_sg_example.train_model(gen_dataset=self.gen_dataset_sg_example, steps=100, metrics=[Metric("Accuracy", mask='test')])
+        mask_loc = Metric.create_mask_by_target_list(y_true=self.gen_dataset_sg_example.labels, target_list=target_list)
+        metric_loc = gnn_model_manager_sg_example.evaluate_model(gen_dataset=self.gen_dataset_sg_example,
+                                                                 metrics=[Metric("F1", mask=mask_loc, average='macro'),
+                                                                          Metric("Accuracy", mask=mask_loc)])
+        print(metric_loc)
+
 
 if __name__ == '__main__':
     unittest.main()
