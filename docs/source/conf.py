057f9da8b2248d7d19e40c18569520023d409603
Misha D <aytm@mail.ru>
2025-05-30 18:08:40 +0300

    fix import for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../../web_interface'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
 
 # -- Project information
