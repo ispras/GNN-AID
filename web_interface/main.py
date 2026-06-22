353423ebff1ed48cb551653e61d4267cea5aa0ca
Misha D <aytm@mail.ru>
2026-06-22 17:31:18 +0300

    fix torch+cuda on frontend

--- web_interface/main.py
+++ web_interface/main.py
@@ -1,3 +1,6 @@
 if __name__ == '__main__':
+    import multiprocessing as mp
+    mp.set_start_method("spawn")
+
     from web_interface.aiohttp_local import run_aiohttp_server
     run_aiohttp_server()
